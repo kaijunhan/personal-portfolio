9a76a78ddc3f44e643326f3609772476f5340fa7
Kai <kaijun@umich.edu>
2024-04-22 01:41:34 -0400

    updated

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
    {
       title: 'Random Website',
-      imgUrl: '/assets/city.png',
+      imgUrl: 'public/assets/city.png',
       stack: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://umich.edu',
    },
    {
       title: 'Random Portfolio',
-      imgUrl: '/assets/bright.png',
+      imgUrl: 'public/assets/bright.png',
       stack: ['React', 'Tailwind', 'Vite'],
       link: 'https://umich.edu',
    },
    {
       title: 'Random Project',
-      imgUrl: '/assets/diner.png',
+      imgUrl: 'public/assets/diner.png',
       stack: ['Python', 'C++', 'Robotics'],
       link: 'https://umich.edu',
    },
